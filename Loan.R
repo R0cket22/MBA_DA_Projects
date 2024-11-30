fe0baca8f30682ccc23c34242e261804c83efdd3
Rishabh Singh <singhrishab2001@gmail.com>
2024-11-30 17:38:11 +0530

    change name

--- Loan.R
+++ Loan.R
@@ -25,27 +25,19 @@ dt_model = rpart(Personal.Loan~., data=train,
 test$prediction1 = predict(dt_model, test, type='class')
 table (test$Personal.Loan, test$prediction1)
 
-81/81+5
-81+5
-81/86
-81+17
-81/98
-81+897+17+5
-81+897
-978/1000
 
-install.packages('e1071')
-library(e1071)
+#install.packages('e1071')
+#ibrary(e1071)
 
 
 
 
-nb_model= naiveBayes(Personal.Loan~., data=train)
-test= test[c(-13)]
-test$prediction_1 = predict(nb_model, test)
-table (test$Personal.Loan, test$prediction_1)
+#nb_model= naiveBayes(Personal.Loan~., data=train)
+#test= test[c(-13)]
+#test$prediction_1 = predict(nb_model, test)
+#table (test$Personal.Loan, test$prediction_1)
 
-install.packages("caret")
-library(caret)
-confusionMatrix(factor(test$Personal.Loan),
-                factor(test$prediction_1))
+#install.packages("caret")
+#library(caret)
+#confusionMatrix(factor(test$Personal.Loan),
+ #               factor(test$prediction_1))
